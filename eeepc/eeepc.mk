c1ba14c0759123571e46a7b15ccf87a2c2b420e2
Chih-Wei Huang <cwhuang@linux.org.tw>
2012-02-23 13:23:54 +0800

    eeepc: enable touchscreen calibration

--- eeepc/eeepc.mk
+++ eeepc/eeepc.mk
@@ -1,4 +1,7 @@
 $(call inherit-product,$(LOCAL_PATH)/../common/generic_asus.mk)
 
+# Get the touchscreen calibration tool
+$(call inherit-product-if-exists,external/tslib/tslib.mk)
+
 PRODUCT_NAME := eeepc
 PRODUCT_DEVICE := eeepc
