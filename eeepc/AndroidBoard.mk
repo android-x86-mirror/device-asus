a1300375e75e9ed93ee3279fdaaca920b4e3339c
Chih-Wei Huang <cwhuang@linux.org.tw>
2012-02-26 23:09:29 +0800

    update kernel configs

--- eeepc/AndroidBoard.mk
+++ eeepc/AndroidBoard.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-LOCAL_FIRMWARES := rt2860.bin $(subst device/common/firmware/,,$(wildcard device/common/firmware/radeon/*))
+LOCAL_FIRMWARES := rt2860.bin $(subst device/common/firmware/,,$(wildcard device/common/firmware/radeon/* device/common/firmware/brcm/*))
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/../common/asus_info
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/eeepc_defconfig
 
